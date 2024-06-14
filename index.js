90ae9966a2f2d727daec2d0d4839aa083ba9638a
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2024-06-14 19:43:54 +0400

    removed old data.json file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const SearchService = require("./SearchService");
 
-const data = require("./small-data.json");
+const data = require("./data.json");
 
 const app = express();
 const port = 3000;
