ca620e0bafe1c9075713b69ce99c7e84dc7e00a0
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2024-06-14 18:45:58 +0400

    added express server for handling query requests

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
+const express = require("express");
 const SearchService = require("./SearchService");
-// const data = require("./data.json");
 
-const data = [
-  {
-    id: 0,
-    title: "Moby Dick",
-    text: "Call me Ishmael. Some years ago...",
-  },
-  {
-    id: 1,
-    title: "Zen and the Art of Motorcycle Maintenance",
-    text: "I can see by my watch...",
-  },
-  {
-    id: 2,
-    title: "Neuromancer",
-    text: "The sky above the port was...",
-  },
-  {
-    id: 3,
-    title: "Zen and the Art of Archery",
-    text: "At first sight it must seem...",
-  },
-  {
-    id: 4,
-    title: "Zen and the Art of Archery",
-    text: "At first sight it must seem...",
-  },
-  {
-    id: 5,
-    title: "sample title",
-    text: "sampel default text...",
-  },
-];
+const smallData = require("./small-data.json");
 
-const searchService = new SearchService(data, { fields: ["title", "text"] });
+const app = express();
+const port = 3000;
 
-searchService.search("fir ");
+const searchService = new SearchService(smallData, {
+  fields: ["title", "text"],
+});
+
+app.get("/search", (req, res) => {
+  const { query } = req;
+  res.send(searchService.search(query.term));
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
